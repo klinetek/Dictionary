3eddfedf31bcd3e949820e03118022005a22311d
Dustin Kline <klinetek@gmail.com>
2021-07-03 22:46:59 -0400

    Adding in close matches and string formatting for entries

--- app1.py
+++ app1.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -6,6 +7,8 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word,data.keys())) > 0:
+        return "Did you mean %s instead?" %get_close_matches(word, data.keys())[0]
     else:
         return "That word doesn't exits, please double check it."
 
